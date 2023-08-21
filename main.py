22024a94d3bedb912271a803c0ca083998306606
Xiaoping Zhang <zhan0566@algonquinlive.com>
2023-08-20 20:00:39 -0400

    few changes

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ if __name__ == '__main__':
     # from business.sqlitebusiness import SQLBusiness
     from presentation.presentation import Presentation
     Presentation().interact()
+
+    print('just for testing')
